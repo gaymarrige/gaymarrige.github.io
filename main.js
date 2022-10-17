5f890b19980fb6177146c930115e331037419b2d
gaymarrige <111293100+gaymarrige@users.noreply.github.com>
2022-10-17 15:18:32 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (navA.classList.toggle('header__nav-row--open')) {
+        navBtnImg.src = "./../img/system/NAVCLOSE.svg";
+    } else {
+        navBtnImg.src = './../img/system/NAV.svg';
+    }
+}
+
+AOS.init();
